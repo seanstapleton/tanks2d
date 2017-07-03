4f74d00e0712b8cd6ad400c099823cf6a4b232dd
Sean Stapleton <seanstapleton100@gmail.com>
2017-07-03 14:16:20 -0500

    Added tilemap

---
+++ assets/tiles_example.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="fun" tilewidth="32" tileheight="32" tilecount="140" columns="14">
+ <image source="../Downloads/phaser-examples-master/examples/assets/tilemaps/tiles/gridtiles.png" width="448" height="320"/>
+</tileset>
